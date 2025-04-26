127db7df0446df0f4f0c0d4282a9a8ded7379450
jfiebig96 <73064904+jfiebig96@users.noreply.github.com>
2025-04-26 18:10:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 
 # Ładowanie danych z secrets
-API_KEY = st.secrets["API_KEY"]
+API_KEY = "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"
 BASE_URL = "https://openrouter.ai/api/v1"
 MODEL = "google/gemma-3b-it:free"
 
