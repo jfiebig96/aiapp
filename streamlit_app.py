d0a9e82b5f0951db70bfb01fedd817e72ab27767
jfiebig96 <73064904+jfiebig96@users.noreply.github.com>
2025-05-17 12:31:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,23 @@ import fitz
 import streamlit as st
 from openai import OpenAI
 import io
+from typing import Optional
+from langchain_openai import ChatOpenAI
+from pydantic import Field, SecretStr
+
+# === Klasa: Obsługa modelu OpenRouter przez LangChain ===
+class ChatOpenRouter(ChatOpenAI):
+    openai_api_key: Optional[SecretStr] = Field(
+        alias="api_key", default_factory=st.secrets["API_KEY"]
+    )
+
+    @property
+    def lc_secrets(self) -> dict[str, str]:
+        return {"openai_api_key": st.secrets["API_KEY"]}
+
+    def __init__(self, openai_api_key: Optional[str] = None, **kwargs):
+        openai_api_key = openai_api_key or st.secrets["API_KEY"]
+        super().__init__(base_url=st.secrets["BASE_URL"], openai_api_key=openai_api_key, **kwargs)
 
 # === Funkcja: wyciąganie tekstu z pojedynczego pliku PDF ===
 def load_pdf_from_file(uploaded_file):
